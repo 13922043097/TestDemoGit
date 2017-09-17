47f9d79ae7ea4ecc7f4ac9bd750767250a991296
xielong <13922043097@163.com>
2017-09-17 17:17:52 +0800

    gittest say hello world!

--- src/com/kit/TestController.java
+++ src/com/kit/TestController.java
@@ -9,6 +9,8 @@ package com.kit;
  */
 public class TestController {
     public static void main(String[] args){
+
+        System.out.println("hello world!");
         System.out.println("hello world!");
     }
 }
