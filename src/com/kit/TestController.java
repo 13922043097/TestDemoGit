46de7d7ea8c8649a35a65b20ef2dd40899b1394b
xielong <13922043097@163.com>
2017-09-17 14:57:17 +0800

    commit 一个测试控制器

---
+++ src/com/kit/TestController.java
@@ -0,0 +1,14 @@
+package com.kit;
+
+/**
+ * @version V1.0
+ * @Title: Controller
+ * @Description:
+ * @author:XieLong
+ * @date 2017/9/17 0017
+ */
+public class TestController {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
