1221f0894bf1ea99630a5704f548393f6bf2363e
xielong <13922043097@163.com>
2017-09-17 14:39:46 +0800

    commit test

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @version V1.0
+ * @Title: Controller
+ * @Description:
+ * @author:XieLong
+ * @date 2017/9/17 0017
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Welcom！");
+    }
+}
